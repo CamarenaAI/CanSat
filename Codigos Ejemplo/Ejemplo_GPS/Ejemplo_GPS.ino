1cba9b9864398f268e2479ebdf910de3b3fe10af
Alonso Ismael Camarena <71803835+CamarenaAI@users.noreply.github.com>
2023-02-16 14:24:27 -0600

    Codigos Ejemplo

---
+++ Codigos Ejemplo/Ejemplo_GPS/Ejemplo_GPS.ino
@@ -0,0 +1,40 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+const int RX1 = 3, TX1 = 4;
+SoftwareSerial vke16(RX1, TX1); // RX=pin 19, TX=pin 18
+TinyGPSPlus gps;                // azul       verde
+
+void setup() {
+  Serial.begin(9600);
+  vke16.begin(9600); // Esto abre las comunicaciones con el GPS.
+
+  Serial.println("Conectando");
+}
+
+void loop() {
+  while (vke16.available()) // Si bien hay caracteres que vienen del GPS
+  {
+    gps.encode(vke16.read());// Esto alimenta los datos seriales NMEA en la biblioteca un carácter a la vez
+  }
+  if (gps.location.isUpdated()) // De todos modos, esto se activará casi todo el tiempo, pero al menos lo reducirá solo después de que ingrese un paquete de datos NMEA
+  {
+    // Obtenga la información más reciente del objeto GPS que se deriva de los datos enviados por la unidad GPS
+    // Serial.println("Satellite Count:");
+    // Serial.println(gps.satellites.value());
+    Serial.print("L: ");
+    Serial.print(gps.location.lat(), 6);
+    // Serial.println(gps.location.lat(), 6);
+
+    Serial.print("l: ");
+    Serial.print(gps.location.lng(), 6);
+    Serial.print(" Speed MPH: ");
+    Serial.print(gps.speed.mph());
+    Serial.print(" A: ");
+    Serial.print(gps.altitude.meters());
+    Serial.println("");
+
+//  Serial.println(" ");
+//  delay(1000);
+  }
+}
